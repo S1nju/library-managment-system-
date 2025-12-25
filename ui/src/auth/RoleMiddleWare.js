be741f73fa2cdda0ab108825eb5246315ed9641b
s1nju <78903844+amine502@users.noreply.github.com>
2025-12-25 15:55:22 +0100

    add:RoleMiddleware to check the user roles

---
+++ ui/src/auth/RoleMiddleWare.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from '../context/UserContext'
+
+function RoleMiddleWare({allowedRole}){ 
+    const {user} = useUser();
+    const [currentUser,setCurrentUser]=React.useState(null);
+
+    React.useEffect(()=>{
+        setCurrentUser(user);
+    },[user]);
+    console.log(currentUser?.user.roles[0].name);
+    console.log((currentUser?.user.roles[0].name === allowedRole[0]));
+  return (currentUser && (currentUser.user.roles.some(r=>allowedRole.includes(r.name)))
+      ? <Outlet />
+      : <></>
+  )
+}
+
+export default RoleMiddleWare
